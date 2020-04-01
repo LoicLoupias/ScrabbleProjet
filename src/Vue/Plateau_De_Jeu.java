10dcfcb6b5ed497a5b6a2bb5f606f922d2d40a10
Thomas Maury <7rchim@gmail.com>
2020-04-01 15:05:55 +0200

    Amélioration des classes concernant le plateau de jeu en créant une structure de données 'colonne'

--- src/Vue/Plateau_De_Jeu.java
+++ src/Vue/Plateau_De_Jeu.java
@@ -1,39 +1,118 @@
+import java.awt.Color;
+import java.awt.Component;
+import java.awt.Dimension;
 import java.awt.Insets;
+import java.awt.event.WindowEvent;
+import java.awt.event.WindowListener;
 
+import javax.swing.BorderFactory;
+import javax.swing.Box;
 import javax.swing.BoxLayout;
 import javax.swing.JButton;
+import javax.swing.JFrame;
 import javax.swing.JPanel;
+import javax.swing.border.Border;
+import javax.swing.border.EmptyBorder;
 
-public class Plateau_De_Jeu extends JPanel {
+public class Plateau_De_Jeu extends JPanel implements WindowListener {
     
-	public Plateau_De_Jeu() {
-		// Set the panel to add buttons
-		super();
+	Column[] colonne;
+	
+	public Plateau_De_Jeu(int taille) {
 		
-		// Set the BoxLayout to be X_AXIS: from left to right		
-		this.setLayout(new BoxLayout(this, BoxLayout.X_AXIS));
+		this.setPreferredSize(new Dimension(50*taille, 50*taille));
 		
-		// Set border for the panel
-		//this.setBorder(new EmptyBorder(new Insets(150, 200, 150, 200)));
-		//panel.setBorder(new EmptyBorder(new Insets(50, 80, 50, 80)));     
+		colonne = new Column[taille];
 		
-		// Define new buttons
-		JButton [] boutonTab = new JButton[15];
-		for (int i = 0; i<15; i++) {
-			boutonTab[i] = new JButton(Integer.toString(i));
-			boutonTab[i].setSize(50, 50);
-			this.add(boutonTab[i]);
+		for (int i = 0; i<taille; i++) {
+			this.colonne[i] = new Column();
+			this.colonne[i].setBorder(BorderFactory.createLineBorder(Color.BLACK, 1));
+			this.add(colonne[i]);
 		}
-     
-    // Add buttons to the frame (and spaces between buttons)
-    //panel.add(Box.createRigidArea(new Dimension(0, 60)));     
-
-     
-    // Set size for the frame
-    //frame.setSize(300, 300);
-     
-    // Set the window to be visible as the default to be false
-
+	    for(int i = 0; i<Vue.MOT_DOUBLE.length; i++) {
+	    	colonne[Vue.MOT_DOUBLE[i][0]].getComponent(Vue.MOT_DOUBLE[i][1]).setBackground(Color.ORANGE);
+	    	colonne[14 - Vue.MOT_DOUBLE[i][0]].getComponent(14 - Vue.MOT_DOUBLE[i][1]).setBackground(Color.ORANGE);
+	    }
+	    for(int i = 0; i<Vue.LETTRE_DOUBLE.length; i++) {
+	    	colonne[Vue.LETTRE_DOUBLE[i][0]].getComponent(Vue.LETTRE_DOUBLE[i][1]).setBackground(Color.CYAN);
+	    	colonne[14 - Vue.LETTRE_DOUBLE[i][0]].getComponent(14 - Vue.LETTRE_DOUBLE[i][1]).setBackground(Color.CYAN);
+	    }
+	    for(int i = 0; i<Vue.MOT_TRIPLE.length; i++) {
+	    	colonne[Vue.MOT_TRIPLE[i][0]].getComponent(Vue.MOT_TRIPLE[i][1]).setBackground(Color.RED);
+	    	colonne[14 - Vue.MOT_TRIPLE[i][0]].getComponent(14 - Vue.MOT_TRIPLE[i][1]).setBackground(Color.RED);
+	    }
+	    for(int i = 0; i<Vue.LETTRE_TRIPLE.length; i++) {
+	    	colonne[Vue.LETTRE_TRIPLE[i][0]].getComponent(Vue.LETTRE_TRIPLE[i][1]).setBackground(Color.BLUE);
+	    	colonne[14 - Vue.LETTRE_TRIPLE[i][0]].getComponent(14 -Vue.LETTRE_TRIPLE[i][1]).setBackground(Color.BLUE);
+	    }
 	}
-}
 
+
+	public class Column extends JPanel {
+		
+		JButton [] boutonTab = new JButton[Vue.SIZE];
+		
+		public Column() {
+			
+			// Set the panel to add buttons
+			super();
+					
+			// Set the BoxLayout to be X_AXIS: from left to right		
+			this.setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
+					
+			// Set size for the frame
+			this.setPreferredSize(new Dimension(50, 50*Vue.SIZE));
+					
+			// Define new buttons
+			for (int i = 0; i<Vue.SIZE; i++) {
+				boutonTab[i] = new JButton(Integer.toString(i));
+				boutonTab[i].setPreferredSize(new Dimension(100, 100));
+				boutonTab[i].setBackground(new Color(249,228,183));
+				this.boutonTab[i].setBorder(BorderFactory.createLineBorder(Color.BLACK));
+				this.add(boutonTab[i]);
+			}
+		}
+	}
+
+
+	public void windowActivated(WindowEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+
+	public void windowClosed(WindowEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+
+	public void windowClosing(WindowEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+
+	public void windowDeactivated(WindowEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+
+	public void windowDeiconified(WindowEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+
+	public void windowIconified(WindowEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+
+	public void windowOpened(WindowEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+}		
--- src/Vue/Vue.java
+++ src/Vue/Vue.java
@@ -1,4 +1,9 @@
+import java.awt.Color;
+import java.awt.Dimension;
+
 import javax.swing.*;
+import javax.swing.border.EmptyBorder;
+
 
 public class Vue extends JFrame {
 
@@ -6,18 +11,45 @@ public class Vue extends JFrame {
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
-
+	
+	static int SIZE = 15;
+	static int[][] MOT_DOUBLE = {{1,1}, {2,2}, {3,3}, {4,4}, {1,13}, {2,12}, {3,11}, {4,10}};
+	static int[][] LETTRE_DOUBLE = {{0,3}, {0,11}, {2,6}, {2,8}, {3,0}, {3,7}, {3,14}, {6,6}, {6,8}};
+	static int[][] MOT_TRIPLE = {{0,0}, {7,7}, {0,14}, {14,14}, {0,7}, {0,7}};
+	static int[][] LETTRE_TRIPLE = {{1,5}, {1,9}, {5,1}, {5,5}, {5,9}, {5,13}};
+	
+	
 	public Vue(String titre) {
 		super(titre);
-	    this.setLayout(new BoxLayout(getContentPane(), BoxLayout.Y_AXIS));
+	    this.setLayout(new BoxLayout(getContentPane(), BoxLayout.X_AXIS));
+	    setResizable(false);
 	    
-	    JPanel[] panel = new Plateau_De_Jeu[15];
-	    for (int i = 0; i<15; i++) {
+	    /* Plateau de jeu
+	    JPanel[] panel = new Plateau_De_Jeu[SIZE];
+	    for (int i = 0; i<SIZE; i++) {
 			panel[i] = new Plateau_De_Jeu();
-			//panel[i].setSize(500, 50);
+			panel[i].setBorder(BorderFactory.createLineBorder(Color.BLACK, 1));
 			this.add(panel[i]);
 		}
-	    
+	    for(int i = 0; i<MOT_DOUBLE.length; i++) {
+	    	panel[MOT_DOUBLE[i][0]].getComponent(MOT_DOUBLE[i][1]).setBackground(Color.ORANGE);
+	    	panel[14 - MOT_DOUBLE[i][0]].getComponent(14 - MOT_DOUBLE[i][1]).setBackground(Color.ORANGE);
+	    }
+	    for(int i = 0; i<LETTRE_DOUBLE.length; i++) {
+	    	panel[LETTRE_DOUBLE[i][0]].getComponent(LETTRE_DOUBLE[i][1]).setBackground(Color.CYAN);
+	    	panel[14 - LETTRE_DOUBLE[i][0]].getComponent(14 - LETTRE_DOUBLE[i][1]).setBackground(Color.CYAN);
+	    }
+	    for(int i = 0; i<MOT_TRIPLE.length; i++) {
+	    	panel[MOT_TRIPLE[i][0]].getComponent(MOT_TRIPLE[i][1]).setBackground(Color.RED);
+	    	panel[14 - MOT_TRIPLE[i][0]].getComponent(14 - MOT_TRIPLE[i][1]).setBackground(Color.RED);
+	    }
+	    for(int i = 0; i<LETTRE_TRIPLE.length; i++) {
+	    	panel[LETTRE_TRIPLE[i][0]].getComponent(LETTRE_TRIPLE[i][1]).setBackground(Color.BLUE);
+	    	panel[14 - LETTRE_TRIPLE[i][0]].getComponent(14 - LETTRE_TRIPLE[i][1]).setBackground(Color.BLUE);
+	    }
+	    */
+	    Plateau_De_Jeu plateau = new Plateau_De_Jeu(SIZE);
+	    this.add(plateau);
 	    this.pack();
 	    this.setVisible(true);
 	    this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);

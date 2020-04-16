e4d98be31e89279fd33b0d54109eb12d8e09fbf1
loic <loic.loupias@outlook.fr>
2020-04-16 11:49:37 +0200

    ajout d'un possible passerTour pour un joueur  avec création de la fonction

--- src/Modele/Joueur.java
+++ src/Modele/Joueur.java
@@ -7,13 +7,17 @@ public class Joueur {
 	public String nom;
 	public int point;
 	public ArrayList<Character> lettre;
+	public int passerTour;
 	
 	public Joueur(String name) {
+		this.passerTour = 0;
+		this.nom = name;
+		this.point = 0;
+		this.lettre = new ArrayList<Character>();
 		
-		nom = name;
-		point = 0;
-		lettre = new ArrayList<Character>();
-		
+	}
+	public void passTour() {
+		this.passerTour ++;
 	}
 	
 }

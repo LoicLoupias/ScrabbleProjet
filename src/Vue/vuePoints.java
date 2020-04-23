f1735dd7a7f100eacdf2e91b7f99ce5dc85623ac
FabRLS <fabienrols@hotmail.fr>
2020-04-23 20:29:18 +0200

    amélioration de vuePoints pour un rendu plus agréable

--- src/Vue/vuePoints.java
+++ src/Vue/vuePoints.java
@@ -6,10 +6,12 @@ import java.awt.FlowLayout;
 import java.awt.Font;
 import java.awt.Label;
 
+import javax.swing.BorderFactory;
 import javax.swing.Box;
 import javax.swing.BoxLayout;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.SwingConstants;
 
 import Modele.Joueur;
 
@@ -23,41 +25,58 @@ public class vuePoints extends JPanel {
 		super();
 		
 		joueur = jou;
-		
-		//this.setPreferredSize(new Dimension(500, 100));
+		this.setBackground(new Color (0,200,0));
+		this.setBorder(BorderFactory.createRaisedBevelBorder());
 		
 		JPanel[] pointJoueur = new JPanel[joueur.length];
 		for (int i = 0; i<joueur.length; i++) {
 			pointJoueur[i] = new JPanel();
 			pointJoueur[i].setLayout(new BoxLayout(pointJoueur[i], BoxLayout.Y_AXIS));
+			pointJoueur[i].setPreferredSize(new Dimension(100,50));
+			
 			JLabel nomJ = new JLabel(joueur[i].nom);
 			Font fontnomJ = new Font("Arial", Font.BOLD,16);
+			nomJ.setAlignmentX(CENTER_ALIGNMENT);
+			nomJ.setFont(fontnomJ);
+			
 			JLabel pointJ = new JLabel(Integer.toString(joueur[i].point));
 			Font fontpointJ = new Font("Arial", Font.BOLD,16);
-			nomJ.setFont(fontnomJ);
 			pointJ.setFont(fontpointJ);
+			pointJ.setAlignmentX(CENTER_ALIGNMENT);
 			pointJoueur[i].add(nomJ);
+			
 			pointJoueur[i].add(pointJ);
+			if (i==0) {
+				pointJoueur[i].setBackground(new Color(231, 62, 1));
+			}else if (i==1){
+				pointJoueur[i].setBackground(new Color(15, 157, 232));
+			} else if (i==2) {
+				pointJoueur[i].setBackground(Color.YELLOW);
+			} else {
+				pointJoueur[i].setBackground(Color.ORANGE);
+			}
+			pointJoueur[i].setBorder(BorderFactory.createRaisedBevelBorder());
+			
 		}
 		
 		JPanel pointTab = new JPanel();
+		pointTab.setBackground(new Color (0,200,0));
 		pointTab.setLayout(new FlowLayout());
-		pointTab.add(Box.createRigidArea(new Dimension(75,10)));
+		pointTab.add(Box.createRigidArea(new Dimension(25,10)));
 		for (int i = 0; i<pointJoueur.length; i++) {
 			pointTab.add(pointJoueur[i]);
-			pointTab.add(Box.createRigidArea(new Dimension(150,10)));
+			pointTab.add(Box.createRigidArea(new Dimension(60,10)));
 		}
 		BoxLayout layoutvuePoints = new BoxLayout(this, BoxLayout.Y_AXIS);
 		//layoutvuePoints.setVgap(5);
 		this.setLayout(layoutvuePoints);
 		
-		JLabel titre = new JLabel("JOUEURS");
-		Font font = new Font("Arial", Font.BOLD,20);
+		JLabel titre = new JLabel("JOUEURS :");
+		Font font = new Font("Arial", Font.BOLD,25);
+		titre.setAlignmentX(CENTER_ALIGNMENT);
 		titre.setFont(font);
-		titre.setHorizontalAlignment(titre.CENTER);
 		
 		this.add(titre);
-		this.add(Box.createRigidArea(new Dimension(0,15)));
 		this.add(pointTab);
 		
 		

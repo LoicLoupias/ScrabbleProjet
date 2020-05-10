ec8a1e30ade5cfe14abe051f9acc603e4dcc8ed7
FabRLS <fabienrols@hotmail.fr>
2020-05-10 18:16:49 +0200

    Amélioration de l'historique

--- src/Vue/vueHistorique.java
+++ src/Vue/vueHistorique.java
@@ -1,107 +1,107 @@
-//Tests poour liste défilante de l'historique
-
-/*package Vue;
-
-import java.awt.Color;
-import java.awt.Font;
-import java.awt.Frame;
-import java.util.ArrayList;
-import javax.swing.BorderFactory;
-import javax.swing.BoxLayout;
-import javax.swing.JLabel;
-import javax.swing.JList;
-import javax.swing.JPanel;
-import javax.swing.JTextArea;
-
-
-
-public class vueHistorique extends JPanel {
-	
-	public ArrayList<JLabel> historique; 
-	public JList listehistorique;
-	
-	public vueHistorique() {
-		
-		super();
-		historique = new ArrayList();
-		listehistorique = new JList(historique.toArray());
-		
-		JLabel titrehistorique = new JLabel("HISTORIQUE DU JEU : ");
-		Font fonthistorique = new Font("Arial", Font.BOLD,25);
-		titrehistorique.setAlignmentX(CENTER_ALIGNMENT);
-		titrehistorique.setFont(fonthistorique);
-		
-		listehistorique.setBackground(Color.LIGHT_GRAY);
-		listehistorique.add(titrehistorique);
-		this.add(listehistorique);
-		
-		this.setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
-		//this.historique = new ArrayList<JLabel>();
-		
-		
-		
-		
-		
-		
-		//this.add(historique.get(0));
-		//this.setBackground(Color.LIGHT_GRAY);
-		this.setBorder(BorderFactory.createRaisedBevelBorder());
-	}
-	
-	public void ajoutHistorique(String mot) {
-		JLabel motjoue = new JLabel(mot);
-		Font fontmotjoue = new Font("Arial", Font.PLAIN, 18);
-		motjoue.setAlignmentX(CENTER_ALIGNMENT);
-		motjoue.setFont(fontmotjoue);
-		this.listehistorique.add(motjoue);
-		//this.add(this.listehistorique.get(this.listehistorique.size() - 1));
-		this.updateUI();//actualise l'affichage de la frame
-		
-		
-	}
-}
-*/
-
-//Bonne version temporaire 
+//Tests pour liste défilante de l'historique
 
 package Vue;
 
 import java.awt.Color;
-import java.util.ArrayList;
+import java.awt.Dimension;
+import java.awt.Font;
+
 import javax.swing.BorderFactory;
 import javax.swing.BoxLayout;
-import javax.swing.JLabel;
 import javax.swing.JPanel;
-
-
+import javax.swing.JScrollPane;
+import javax.swing.JTextArea;
 
 public class vueHistorique extends JPanel {
-	
-	public ArrayList<JLabel> historique; 
-	
+
+	public JScrollPane historique;
+	public JTextArea historiquetexte;
+	public JTextArea titrehistorique;
+	String jeu;
+	String titre;
+	String texte;
+
 	public vueHistorique() {
-		
+
 		super();
-		
+		titre = "                                     HISTORIQUE DU JEU : ";//Adapter le nombre d'espaces en fonction de celui qui fait la vidéo
+		jeu = " ";
+
+		titrehistorique = new JTextArea();
+
+		historiquetexte = new JTextArea();
+		historiquetexte.setEditable(false);
+		historique = new JScrollPane(historiquetexte, JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED,
+				JScrollPane.HORIZONTAL_SCROLLBAR_AS_NEEDED);
+
+		this.add(titrehistorique);
+		this.add(historique);
+
+		Font fonttitrehistorique = new Font("Arial", Font.BOLD, 25);
+		Font fonthistorique = new Font("Arial", Font.PLAIN, 18);
+
+		titrehistorique.setFont(fonttitrehistorique);
+		historiquetexte.setFont(fonthistorique);
+
+		titrehistorique.setBackground(Color.LIGHT_GRAY);
+		historiquetexte.setBackground(Color.LIGHT_GRAY);
+
+		titrehistorique.setText(titre);
+		historiquetexte.setText(jeu);
+
 		this.setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
-		this.historique = new ArrayList<JLabel>();
-		historique.add(new JLabel("HISTORIQUE"));
-		this.add(historique.get(0));
-		this.setBackground(Color.LIGHT_GRAY);
+
 		this.setBorder(BorderFactory.createRaisedBevelBorder());
-		
-		//this.setPreferredSize(new Dimension(500, 100));
-		
-		
-		
 	}
-	
+
 	public void ajoutHistorique(String mot) {
-		
-		this.historique.add(new JLabel(mot));
-		this.add(this.historique.get(this.historique.size() - 1));
-		this.updateUI();//actualise l'affichage de la frame
-		
-		
+		if (mot.charAt(0) == 'C') {
+			jeu = jeu + mot + "\n \n";
+		} else {
+			jeu = jeu + mot + " \n";
+		}
+		this.historiquetexte.setText(jeu);
+		this.updateUI();// actualise l'affichage de la frame
+
 	}
 }
+
+//Bonne version temporaire 
+
+/*
+ * package Vue;
+ * 
+ * import java.awt.Color; import java.util.ArrayList; import
+ * javax.swing.BorderFactory; import javax.swing.BoxLayout; import
+ * javax.swing.JLabel; import javax.swing.JPanel;
+ * 
+ * 
+ * 
+ * public class vueHistorique extends JPanel {
+ * 
+ * public ArrayList<JLabel> historique;
+ * 
+ * public vueHistorique() {
+ * 
+ * super();
+ * 
+ * this.setLayout(new BoxLayout(this, BoxLayout.Y_AXIS)); this.historique = new
+ * ArrayList<JLabel>(); historique.add(new JLabel("HISTORIQUE"));
+ * this.add(historique.get(0)); this.setBackground(Color.LIGHT_GRAY);
+ * this.setBorder(BorderFactory.createRaisedBevelBorder());
+ * 
+ * //this.setPreferredSize(new Dimension(500, 100));
+ * 
+ * 
+ * 
+ * }
+ * 
+ * public void ajoutHistorique(String mot) {
+ * 
+ * this.historique.add(new JLabel(mot));
+ * this.add(this.historique.get(this.historique.size() - 1));
+ * this.updateUI();//actualise l'affichage de la frame
+ * 
+ * 
+ * } }
+ */
